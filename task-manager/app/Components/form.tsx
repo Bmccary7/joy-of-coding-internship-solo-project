588014e626fe977bd650cb91417cd02b4ec85dfe
Brandon McCary <mccarymid7@gmail.com>
2024-10-31 12:31:36 -0500

    Implement error handling and confirmation for task creation succes/failure

--- task-manager/app/Components/form.tsx
+++ task-manager/app/Components/form.tsx
@@ -1,12 +1,15 @@
-import { Button, DropdownMenu, Flex, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, DropdownMenu, Flex, TextArea, TextField } from '@radix-ui/themes'
 import { useRouter } from 'next/navigation';
 import React, { useRef, useState } from 'react'
 import { createTask } from '../api/route';
+import { createTaskSchema } from '../validationSchemas';
 
 const Form = () => {
 
     const priorityList = ['Low', 'Moderate', 'High'];
     const [priorityValue, setPriorityValue] = useState('');
+    const [error, setError] = useState('');
+    const [taskSuccess, setTaskSuccess] = useState('');
     const router = useRouter();
     const ref = useRef<HTMLFormElement>(null)
 
@@ -17,13 +20,56 @@ const Form = () => {
     }
 
     const submitTask = async (formData: FormData) => {
-      await createTask(formData)
-      ref.current?.reset()
-      setPriorityValue('');
-      console.log("submitted task!")
+      setError('');
+      setTaskSuccess('');
+
+      const newTask = {
+        title: formData.get("title"),
+        description: formData.get("description"),
+        priority: formData.get("priority")
+      }
+
+      const result = createTaskSchema.safeParse(newTask);
+
+      let errorMessage = "";
+
+      if (!result.success){
+        result.error.issues.forEach((issue) => {
+          errorMessage = errorMessage + issue.path[0] + ", ";
+          setError("The following fields are required: " + errorMessage.slice(0, errorMessage.length-2))
+        })
+      }else{
+        try {
+          await createTask(formData);
+          ref.current?.reset();
+          setPriorityValue('');
+          setTaskSuccess("Task created successfully!");
+        } catch (error) {
+          setError("An unexpected error occurred.") 
+        }
+      }
+
+      // await createTask(formData)
+      // ref.current?.reset()
+      // setPriorityValue('');
+      // console.log("submitted task!")
     }
   return (
     <div className='max-w-xl'>
+      {error && 
+        <Callout.Root className='mb-5' color='red'>
+          <Callout.Text>
+            {error}
+          </Callout.Text>
+        </Callout.Root>
+      }
+      {taskSuccess &&
+        <Callout.Root className='mb-5' color='green'>
+          <Callout.Text>
+            {taskSuccess}
+          </Callout.Text>
+        </Callout.Root>
+      }
       <form
         ref={ref}
         action={submitTask}

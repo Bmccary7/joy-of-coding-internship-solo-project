3228e28a12feb0cb9252c4ba2188e9644077bd36
Brandon McCary <mccarymid7@gmail.com>
2024-11-01 10:49:53 -0500

    Reset textfields on closing the popover window for editing

--- task-manager/app/Components/EditBtn.tsx
+++ task-manager/app/Components/EditBtn.tsx
@@ -1,22 +1,39 @@
 'use client'
 
 import { Box, Button, DropdownMenu, Flex, Popover, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const EditBtn = () => {
     const prioList = ["Low", "Moderate", "High"];
+    const [prioValue, setPrioValue] = useState('');
+    const [descValue, setDescValue] = useState('');
+    const [titleValue, setTitleValue] = useState('');
+    const ref = useRef<HTMLFormElement>(null);
+
+    const handleSelect = (data: any) => {
+        if (data != null){
+            setPrioValue(data);
+        }
+    }
+
+    const resetFields = () => {
+        ref.current?.reset();
+        setPrioValue('');
+    }
+
   return (
     <div>
         <Popover.Root>
             <Popover.Trigger>
                 <Button>Edit</Button>
             </Popover.Trigger>
-            <Popover.Content>
+            <Popover.Content onCloseAutoFocus={resetFields}>
                 <Flex>
                     <Box>
-                        <form>
+                        <form ref={ref}>
                             <Flex>
                                 <p>Currently Editing:</p>
+                                <Button type='button'onClick={() => console.log(prioValue)}>RESET</Button>
                             </Flex>
                             <TextArea placeholder='Edit title'></TextArea>
                             <TextArea placeholder='Edit description'></TextArea>
@@ -27,12 +44,23 @@ const EditBtn = () => {
                                     </DropdownMenu.Trigger>
                                     <DropdownMenu.Content>
                                         {prioList.map((item) => 
-                                            <DropdownMenu.Item key={item}>{item}</DropdownMenu.Item>
+                                            <DropdownMenu.Item 
+                                                key={item}
+                                                onSelect={() => handleSelect(item)}>{item}
+                                            </DropdownMenu.Item>
                                         )}
                                     </DropdownMenu.Content>
                                 </DropdownMenu.Root>
                             </Flex>
-                            <TextField.Root readOnly></TextField.Root>
+                            <TextField.Root readOnly value={prioValue}></TextField.Root>
+                            <Button asChild>
+                                <Popover.Close>
+                                    <Button>Confirm</Button>
+                                </Popover.Close>
+                            </Button>
+                            <Popover.Close>
+                                <Button variant='soft' color='gray'>Cancel</Button>
+                            </Popover.Close>
                         </form>
                     </Box>
                 </Flex>
